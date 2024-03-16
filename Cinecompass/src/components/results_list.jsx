757891c8a59b52551d33c95cdca5e43609b842da
Sarp Bozkurt <sarpbozk@gmail.com>
2024-03-16 07:27:10 +0300

    Fix/networking (#5)
    * fix api
    
    * fix android crash

--- Cinecompass/src/components/results_list.jsx
+++ Cinecompass/src/components/results_list.jsx
@@ -6,17 +6,22 @@ import colors from "../constants/colors";
 const ResultsList = ({ title, results }) => {
   const { titleStyle } = styled(colors.dark_theme);
   return (
-    <View>
-      <Text style={titleStyle}>{title}</Text>
-      <Text>Results: {results.length}</Text>
-      <FlatList
-        data={results}
-        keyExtractor={(result) => result.id}
-        renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
-        }}
-      />
-    </View>
+    <>
+      {results && (
+        <View>
+          <Text style={titleStyle}>{title}</Text>
+          <Text>Results: {results.length}</Text>
+
+          <FlatList
+            data={results}
+            keyExtractor={(result) => result.id}
+            renderItem={({ item }) => {
+              return <Text>{item.title}</Text>;
+            }}
+          />
+        </View>
+      ) }
+    </>
   );
 };
 const styled = (themePalette) =>
