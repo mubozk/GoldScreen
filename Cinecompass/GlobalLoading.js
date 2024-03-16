2f8bb62dfbf059d40dbe8d23984851acac8819ec
Sarp <sarpbozk@gmail.com>
2024-03-16 11:16:20 +0300

    remove unnecessary styling

--- Cinecompass/GlobalLoading.js
+++ Cinecompass/GlobalLoading.js
@@ -13,18 +13,5 @@ const GlobalLoading = () => {
   return <LoadingView />;
 };
 
-const styles = StyleSheet.create({
-  loadingContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    backgroundColor: "rgba(0, 0, 0, 0.5)", // Semi-transparent background
-  },
-});
 
 export default GlobalLoading;
